3037127fdd27277b16e62f4c33fc7e5bf74e1a13
askaaaaaaaaaa _ <bolbagantunahan22@gmail.com>
2023-10-19 11:03:23 +0700

    add intent with data

--- app/src/main/java/com/damarazka/intentmas/MainActivity.kt
+++ app/src/main/java/com/damarazka/intentmas/MainActivity.kt
@@ -11,15 +11,25 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val btnMoveActivities : Button = findViewById(R.id.btn_move_activities)
+        val btnMoveActivities: Button = findViewById(R.id.btn_move_activities)
         btnMoveActivities.setOnClickListener(this)
+
+        val btnMoveWithData: Button = findViewById(R.id.btn_move_activity_data)
+        btnMoveWithData.setOnClickListener(this)
     }
 
     override fun onClick(view: View?) {
-        when(view?.id){
-            R.id.btn_move_activities->{
-                val moveIntent = Intent(this@MainActivity,MoveActivity::class.java)
+        when (view?.id) {
+            R.id.btn_move_activities -> {
+                val moveIntent = Intent(this@MainActivity, MoveActivity::class.java)
                 startActivity(moveIntent)
+            }
+
+            R.id.btn_move_activity_data -> {
+                val moveWithDataIntent = Intent(this@MainActivity, MoveWithDataActivity::class.java)
+                moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_NAME, "azka jawa boy")
+                moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_AGE, 18)
+                startActivity(moveWithDataIntent)
             }
         }
     }
